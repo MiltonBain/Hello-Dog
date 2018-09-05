a27be623a4ce72f25e902e99b65d1af873ac2c6d
Milton Bain <mbain@una.edu>
2018-09-05 22:00:11 +0000

    house cleaning

--- hello.js
+++ hello.js
@@ -9,8 +9,4 @@ http.createServer(function(request, response) {
             + "</html>";
     response.write(html);
     response.end();
-<<<<<<< HEAD
 }).listen(8080);
-=======
-}).listen(8080);
->>>>>>> 54ccf04fd6b6387f09e20d382710648b97616394
