d99ed0d64bb1124a47363f7e7859be9710da40d1
Milton Bain <mbain@una.edu>
2018-09-05 21:40:50 +0000

    script

---
+++ hello.js
@@ -0,0 +1,12 @@
+http.createServer(function(request, response) {
+ response.writeHead(200);
+ let html = "<html>"
+            + "<head>"
+            + "</head>"
+            + "<body>"
+            + "<p>hello this is your dog.</p>"
+            + "</body>"
+            + "</html>";
+    response.write(html);
+    response.end();
+}).listen(8080);
