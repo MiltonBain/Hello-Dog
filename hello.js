2f41b207b29a528dde1a77bc0a5dddd9f29030f1
Milton Bain <mbain@una.edu>
2018-09-06 03:10:34 +0000

    added css

--- hello.js
+++ hello.js
@@ -1,13 +1,48 @@
-let http = require('http');
-http.createServer(function(request, response) {
- response.writeHead(200);
- let html = "<html>"
+const http = require('http');
+http.createServer(function(req, res) {
+  res.writeHead(200,  {'Content-type':'text/html'});
+  let html = "<html>"
             + "<head>"
+            + "<style>"
+            + "body {"
+            + "font-family: Georgia, serif;"
+            + "font-size: 1.25em;"
+            + "background-color: #FDF3E7;"
+            +  "}"
+            + "h1 {"
+            + "text-align: center;"
+            + "border-width: 1px;"
+            + "border-style: solid;"
+            + "border-radius: 10px;"
+            + "background-color: lightblue;"
+            + "}"
+            + ".indent {"
+            + "text-indent: 2em;"
+            + "}"
+            + ".center {"
+            + "text-align: center;"
+            + "margin: 25px;"
+            + "font-weight: bold;"
+            + "}"
+            + "ul li {"
+            + "padding: .25em;"
+            + "margin:10px 0;"
+            + "}"
+            + "</style>"
+            + "<title> Hello from Dog</title>"
             + "</head>"
             + "<body>"
-            + "<p>hello this is your dog.</p>"
+            + "<h1>Can Dogs Speak?</h1>"
+            + "<p></p>"
+            + "<p class=\"center\"> Hello, this is your dog.</p>"
             + "</body>"
             + "</html>";
-    response.write(html);
-    response.end();
-}).listen(8080);
+  res.write(html);
+  res.end();
+}).listen(8080, (err) => {
+  if(err){
+     console.log(err);
+  }
+  
+  console.log("Server is running on port 8080");
+});
